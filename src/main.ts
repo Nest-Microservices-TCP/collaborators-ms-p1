16c1912086094e1708b1e8ed060908fdc92fc133
JosueEC <josueev.cruz@gmail.com>
2024-08-05 19:47:28 -0600

    [REFACTOR](main.ts): Up the app as TCP microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Collaborators-MS');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: envs.collaboratorsHostMS,
+        port: envs.collaboratorsPortMS,
+      },
+    },
+  );
+
+  await app.listen();
+
+  logger.log(`Collaborators Microservice listen on port ${envs.port}`);
 }
 bootstrap();
