5a333480b696c0ce16f643db020ec40214af0929
JosueEC <josueev.cruz@gmail.com>
2024-08-13 22:02:04 -0600

    [FEAT](envs.ts): Add the database envs validations

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -6,6 +6,13 @@ interface EnvVars {
 
   COLLABORATORS_HOST_MS: string;
   COLLABORATORS_PORT_MS: number;
+
+  POSTGRES_DB_HOST: string;
+  POSTGRES_DB_PORT: number;
+  POSTGRES_DB_USERNAME: string;
+  POSTGRES_DB_PASSWORD: string;
+  POSTGRES_DATABASE: string;
+  POSTGRES_DB_SYNCHRONIZE: boolean;
 }
 
 const envSchema = joi
@@ -14,6 +21,13 @@ const envSchema = joi
 
     COLLABORATORS_HOST_MS: joi.string().required(),
     COLLABORATORS_PORT_MS: joi.number().required(),
+
+    POSTGRES_DB_HOST: joi.string().required(),
+    POSTGRES_DB_PORT: joi.number().required(),
+    POSTGRES_DB_USERNAME: joi.string().required(),
+    POSTGRES_DB_PASSWORD: joi.string().required(),
+    POSTGRES_DATABASE: joi.string().required(),
+    POSTGRES_DB_SYNCHRONIZE: joi.boolean().required(),
   })
   .unknown(true);
 
@@ -30,4 +44,11 @@ export const envs = {
 
   collaboratorsHostMS: envVars.COLLABORATORS_HOST_MS,
   collaboratorsPortMS: envVars.COLLABORATORS_PORT_MS,
+
+  postgresDBHost: envVars.POSTGRES_DB_HOST,
+  postgresDBPort: envVars.POSTGRES_DB_PORT,
+  postgresDBUsername: envVars.POSTGRES_DB_USERNAME,
+  postgresDBPassword: envVars.POSTGRES_DB_PASSWORD,
+  postgresDatabase: envVars.POSTGRES_DATABASE,
+  postgresDBSynchronize: envVars.POSTGRES_DB_SYNCHRONIZE,
 };
