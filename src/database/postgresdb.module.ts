5cfcde99876390e552d3860587d493d4b0ccf56c
JosueEC <josueev.cruz@gmail.com>
2024-08-13 22:02:35 -0600

    [FEAT](database): Add the postgres module

---
+++ src/database/postgresdb.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { envs } from 'src/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: envs.postgresDBHost,
+      port: envs.postgresDBPort,
+      username: envs.postgresDBUsername,
+      password: envs.postgresDBPassword,
+      database: envs.postgresDatabase,
+      entities: [],
+      synchronize: envs.postgresDBSynchronize,
+    }),
+  ],
+})
+export class PostgresDBModule {}
