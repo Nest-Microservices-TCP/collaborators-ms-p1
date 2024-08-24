31f506175bfd8697b4e546bcd89c6bdeae74f733
JosueEC <josueev.cruz@gmail.com>
2024-08-23 20:13:40 -0600

    [FEAT](collaborators/repositories): Add the custom collaborators repository

---
+++ src/collaborators/repositories/collaborators.repository.ts
@@ -0,0 +1,50 @@
+import { DataSource, QueryRunner, Repository } from 'typeorm';
+import { CreateCollaboratorDto } from '../dto/create-collaborator.dto';
+import { CollaboratorEntity } from '../entities/collaborator.entity';
+import { ICollaboratorsRepository } from './interfaces/collaborators.repository.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+
+export class CollaboratorsRepository implements ICollaboratorsRepository {
+  private collaboratorsRepository: Repository<CollaboratorEntity>;
+
+  constructor(
+    @InjectRepository(CollaboratorEntity)
+    private readonly defaultRepository: Repository<CollaboratorEntity>,
+    private readonly dataSource: DataSource,
+  ) {
+    this.collaboratorsRepository = this.defaultRepository;
+  }
+
+  setQueryRunner(queryRunner: QueryRunner) {
+    if (queryRunner) {
+      this.collaboratorsRepository =
+        queryRunner.manager.getRepository(CollaboratorEntity);
+    } else {
+      this.collaboratorsRepository = this.defaultRepository;
+    }
+  }
+
+  findAll(): Promise<CollaboratorEntity[]> {
+    return this.collaboratorsRepository.find();
+  }
+
+  findOneById(id: string): Promise<CollaboratorEntity> {
+    return this.collaboratorsRepository.findOne({ where: { id } });
+  }
+
+  create(request: Partial<CollaboratorEntity>): CollaboratorEntity {
+    return this.collaboratorsRepository.create(request);
+  }
+
+  save(request: CreateCollaboratorDto): Promise<CollaboratorEntity> {
+    return this.collaboratorsRepository.save(request);
+  }
+
+  async deletedById(id: string): Promise<CollaboratorEntity> {
+    const collaborator = await this.findOneById(id);
+
+    await this.collaboratorsRepository.delete(id);
+
+    return collaborator;
+  }
+}
