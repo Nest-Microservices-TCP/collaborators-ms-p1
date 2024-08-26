61960a47a2749dda316664640fe3cd6aaaee70a3
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:34:52 -0600

    [FEAT](main.ts): Activate the global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config';
 
@@ -16,6 +16,13 @@ async function bootstrap() {
         port: envs.collaboratorsPortMS,
       },
     },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   await app.listen();
