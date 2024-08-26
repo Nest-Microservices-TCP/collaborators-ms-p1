31e4cac54bc7cf58446a5bca7f3ac6b420ed86c8
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:37:06 -0600

    [FEAT](create-area.dto): Define the properties dto

--- src/areas/dto/create-area.dto.ts
+++ src/areas/dto/create-area.dto.ts
@@ -1,1 +1,8 @@
-export class CreateAreaDto {}
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateAreaDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  name: string;
+}
