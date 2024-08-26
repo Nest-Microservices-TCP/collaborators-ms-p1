2f7027162fafb3b9e3cb7b9293725fe59292ded7
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:36:41 -0600

    [FEAT](areas.module): Import the typeorm-module and the unit-of-work service

--- src/areas/areas.module.ts
+++ src/areas/areas.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AreasController } from './areas.controller';
 import { AreasService } from './areas.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AreaEntity } from './entities/area.entity';
+import { UnitOfWork } from 'src/common/unit-of-work/unit-of-work.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AreaEntity])],
   controllers: [AreasController],
-  providers: [AreasService],
+  providers: [AreasService, UnitOfWork],
 })
 export class AreasModule {}
