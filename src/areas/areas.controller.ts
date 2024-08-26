6d1e13394e73fc117c9b404626f46fb5342aed0a
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:36:05 -0600

    [FEAT](areas.controller): Expose the save method

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -1,4 +1,15 @@
 import { Controller } from '@nestjs/common';
+import { AreasService } from './areas.service';
+import { CreateAreaDto } from './dto';
+import { AreaEntity } from './entities/area.entity';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
-export class AreasController {}
+export class AreasController {
+  constructor(private readonly areasService: AreasService) {}
+
+  @MessagePattern({ cmd: 'save.area' })
+  async save(request: CreateAreaDto): Promise<AreaEntity> {
+    return this.areasService.save(request);
+  }
+}
