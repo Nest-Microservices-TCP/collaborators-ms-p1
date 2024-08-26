34c04cc7355d8ae24c29561e9fa37bcb40048f2f
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:37:43 -0600

    [FEAT](area.entity): Define de properties entity class

--- src/areas/entities/area.entity.ts
+++ src/areas/entities/area.entity.ts
@@ -1,3 +1,13 @@
 import { BaseEntity } from 'src/common/entity/BaseEntity';
+import { Column, Entity } from 'typeorm';
 
-export class AreaEntity extends BaseEntity {}
+@Entity({ name: 'areas' })
+export class AreaEntity extends BaseEntity {
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  name: string;
+}
