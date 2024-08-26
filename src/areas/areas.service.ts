aae5685fa35ac3f70717df2ca8e92769ea33d169
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:35:36 -0600

    [FEAT](areas.service): Add the sabe method and inject the unit-of-work service

--- src/areas/areas.service.ts
+++ src/areas/areas.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { UnitOfWork } from 'src/common/unit-of-work/unit-of-work.service';
+import { AreaEntity } from './entities/area.entity';
+import { CreateAreaDto } from './dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class AreasService {}
+export class AreasService {
+  constructor(private readonly unitOfWork: UnitOfWork) {}
+
+  async save(request: CreateAreaDto): Promise<AreaEntity> {
+    let newArea: AreaEntity;
+
+    try {
+      await this.unitOfWork.start();
+
+      await this.unitOfWork.complete(async () => {
+        const areasRepository = this.unitOfWork.getAreasRepository();
+
+        newArea = await areasRepository.save(request);
+      });
+
+      return newArea;
+    } catch (error) {
+      throw new RpcException({
+        code: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Error to save the area: ${error}`,
+      });
+    }
+  }
+}
