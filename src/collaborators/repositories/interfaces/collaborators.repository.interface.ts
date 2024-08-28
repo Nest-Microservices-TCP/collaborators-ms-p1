2202229c1fdc02e65eab6129dd81f45e430a1ecb
JosueEC <josueev.cruz@gmail.com>
2024-08-27 22:59:53 -0600

    [FEAT](collaborators.repository): Add the set-query-runner method

--- src/collaborators/repositories/interfaces/collaborators.repository.interface.ts
+++ src/collaborators/repositories/interfaces/collaborators.repository.interface.ts
@@ -1,7 +1,9 @@
 import { CreateCollaboratorDto } from 'src/collaborators/dto/create-collaborator.dto';
 import { CollaboratorEntity } from 'src/collaborators/entities/collaborator.entity';
+import { QueryRunner } from 'typeorm';
 
 export interface ICollaboratorsRepository {
+  setQueryRunner(queryRunner: QueryRunner): void;
   findAll(): Promise<CollaboratorEntity[]>;
   findOneById(id: string): Promise<CollaboratorEntity>;
   create(request: Partial<CollaboratorEntity>): CollaboratorEntity;
