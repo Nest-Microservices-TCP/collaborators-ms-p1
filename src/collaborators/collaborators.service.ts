eebbb6fb1340e38a000da9df21a5f42d4f1e9d08
JosueEC <josueev.cruz@gmail.com>
2024-08-27 22:58:30 -0600

    [FEAT](collaborators.service): Add the find-all method

--- src/collaborators/collaborators.service.ts
+++ src/collaborators/collaborators.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { CollaboratorsRepository } from './repositories/collaborators.repository';
+import { CollaboratorEntity } from './entities/collaborator.entity';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
-export class CollaboratorsService {}
+export class CollaboratorsService {
+  constructor(
+    private readonly collaboratorsRepository: CollaboratorsRepository,
+  ) {}
+
+  async findAll(): Promise<CollaboratorEntity[]> {
+    try {
+      return this.collaboratorsRepository.findAll();
+    } catch (error) {
+      throw new RpcException({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: `Error to get all collaborators: ${error}`,
+      });
+    }
+  }
+}
