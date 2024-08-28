6368faf5549117d2686cee5c04948b491f145b3a
JosueEC <josueev.cruz@gmail.com>
2024-08-27 22:56:02 -0600

    [FEAT](areas.controller): Get the request from the payload

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -2,14 +2,14 @@ import { Controller } from '@nestjs/common';
 import { AreasService } from './areas.service';
 import { CreateAreaDto } from './dto';
 import { AreaEntity } from './entities/area.entity';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AreasController {
   constructor(private readonly areasService: AreasService) {}
 
   @MessagePattern({ cmd: 'save.area' })
-  async save(request: CreateAreaDto): Promise<AreaEntity> {
+  async save(@Payload() request: CreateAreaDto): Promise<AreaEntity> {
     return this.areasService.save(request);
   }
 }
