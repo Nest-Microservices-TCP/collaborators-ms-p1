4efb35f5ff88de469985848f769bf93830c30e73
JosueEC <josueev.cruz@gmail.com>
2024-08-27 23:01:40 -0600

    [FEAT](collaborators.seed): Define the records for the seed

--- src/database/seeds/collaborators.seed.sql
+++ src/database/seeds/collaborators.seed.sql
@@ -2,3 +2,9 @@ TRUNCATE collaborators;
 
 INSERT INTO public.collaborators
 ("name", last_name, in_turn, email, created_by, updated_by, status, deleted_at)
+VALUES
+  ('Fulano', 'Perez Perez', true, 'fulano@gmail.com', NULL, NULL, 'active', NULL),
+  ('Mengano', 'Cruz Cruz', true, 'mengano@gmail.com', NULL, NULL, 'active', NULL),
+  ('Perengano', 'Sosa Sosa', true, 'perengano@gmail.com', NULL, NULL, 'active', NULL),
+  ('Sotorano', 'Martinez Martinez', true, 'sotorano@gmail.com', NULL, NULL, 'active', NULL),
+  ('Jilano', 'Mota Mota', true, 'jilano@gmail.com', NULL, NULL, 'active', NULL),
