1b163e96289949040376e7730929aedb318ce928
JosueEC <josueev.cruz@gmail.com>
2024-08-27 22:58:01 -0600

    [FEAT](collaborators.module): Add the typeorm-module to the imports array

--- src/collaborators/collaborators.module.ts
+++ src/collaborators/collaborators.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CollaboratorsController } from './collaborators.controller';
 import { CollaboratorsService } from './collaborators.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CollaboratorEntity } from './entities/collaborator.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CollaboratorEntity])],
   controllers: [CollaboratorsController],
   providers: [CollaboratorsService],
 })
