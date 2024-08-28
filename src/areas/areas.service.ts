1f91077d40fda13c6f85489f714a9a3fecf5d567
JosueEC <josueev.cruz@gmail.com>
2024-08-27 22:56:50 -0600

    [REFACTOR](areas.service): Use only the repository instead the unit-of-work service

--- src/areas/areas.service.ts
+++ src/areas/areas.service.ts
@@ -1,26 +1,16 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
-import { UnitOfWork } from 'src/common/unit-of-work/unit-of-work.service';
 import { AreaEntity } from './entities/area.entity';
 import { CreateAreaDto } from './dto';
 import { RpcException } from '@nestjs/microservices';
+import { AreasRepository } from './repositories/areas.repository';
 
 @Injectable()
 export class AreasService {
-  constructor(private readonly unitOfWork: UnitOfWork) {}
+  constructor(private readonly areasRepository: AreasRepository) {}
 
   async save(request: CreateAreaDto): Promise<AreaEntity> {
-    let newArea: AreaEntity;
-
     try {
-      await this.unitOfWork.start();
-
-      await this.unitOfWork.complete(async () => {
-        const areasRepository = this.unitOfWork.getAreasRepository();
-
-        newArea = await areasRepository.save(request);
-      });
-
-      return newArea;
+      return await this.areasRepository.save(request);
     } catch (error) {
       throw new RpcException({
         code: HttpStatus.INTERNAL_SERVER_ERROR,
