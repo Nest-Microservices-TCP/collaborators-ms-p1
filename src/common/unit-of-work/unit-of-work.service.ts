ad462ac204427f2d93b4bf8ea0a3f7bb98f098af
JosueEC <josueev.cruz@gmail.com>
2024-08-27 23:00:33 -0600

    [FEAT](unit-of-work.service): Inject and return the collaborators repository

--- src/common/unit-of-work/unit-of-work.service.ts
+++ src/common/unit-of-work/unit-of-work.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { DataSource, QueryRunner } from 'typeorm';
 import { IUnitForWork } from './interfaces/unit-of-work.interface';
 import { AreasRepository } from 'src/areas/repositories/areas.repository';
+import { CollaboratorsRepository } from 'src/collaborators/repositories/collaborators.repository';
 
 @Injectable()
 export class UnitOfWork implements IUnitForWork {
@@ -9,6 +10,7 @@ export class UnitOfWork implements IUnitForWork {
 
   constructor(
     private readonly dataSource: DataSource,
+    private readonly collaboratorsRepository: CollaboratorsRepository,
     private readonly areasRepository: AreasRepository,
   ) {
     this.queryRunner = this.dataSource.createQueryRunner();
@@ -17,6 +19,9 @@ export class UnitOfWork implements IUnitForWork {
   async start(): Promise<void> {
     await this.queryRunner.connect();
     await this.queryRunner.startTransaction();
+
+    this.collaboratorsRepository.setQueryRunner(this.queryRunner);
+    this.areasRepository.setQueryRunner(this.queryRunner);
   }
 
   async complete(work: () => Promise<void>): Promise<void> {
@@ -33,5 +38,9 @@ export class UnitOfWork implements IUnitForWork {
 
   getAreasRepository(): AreasRepository {
     return this.areasRepository;
+  }
+
+  getCollaboratorsRepository(): CollaboratorsRepository {
+    return this.collaboratorsRepository;
   }
 }
