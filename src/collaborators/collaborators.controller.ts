7c5b98467e14bc8441b27083f286bd57f5cc42f5
JosueEC <josueev.cruz@gmail.com>
2024-08-27 22:57:21 -0600

    [FEAT](collaborators.controller): Expose the find-all method

--- src/collaborators/collaborators.controller.ts
+++ src/collaborators/collaborators.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { CollaboratorsService } from './collaborators.service';
+import { CollaboratorEntity } from './entities/collaborator.entity';
 
 @Controller()
-export class CollaboratorsController {}
+export class CollaboratorsController {
+  constructor(private readonly collaboratorsService: CollaboratorsService) {}
+
+  async findAll(): Promise<CollaboratorEntity[]> {
+    return this.collaboratorsService.findAll();
+  }
+}
