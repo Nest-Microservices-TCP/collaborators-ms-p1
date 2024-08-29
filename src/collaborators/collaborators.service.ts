e72ea9822ddd747a203266a9d4b22037876b7f43
JosueEC <josueev.cruz@gmail.com>
2024-08-28 18:36:28 -0600

    [FEAT](collaborators.service): Add the findOneById method

--- src/collaborators/collaborators.service.ts
+++ src/collaborators/collaborators.service.ts
@@ -2,6 +2,7 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 import { CollaboratorsRepository } from './repositories/collaborators.repository';
 import { CollaboratorEntity } from './entities/collaborator.entity';
 import { RpcException } from '@nestjs/microservices';
+import { FindOneCollaboratorById } from './dto/find-one-collaborator-by-id.dto';
 
 @Injectable()
 export class CollaboratorsService {
@@ -16,6 +17,21 @@ export class CollaboratorsService {
       throw new RpcException({
         status: HttpStatus.INTERNAL_SERVER_ERROR,
         message: `Error to get all collaborators: ${error}`,
+      });
+    }
+  }
+
+  async findOneById(
+    request: FindOneCollaboratorById,
+  ): Promise<CollaboratorEntity> {
+    const { collaborator_id } = request;
+
+    try {
+      return await this.collaboratorsRepository.findOneById(collaborator_id);
+    } catch (error) {
+      throw new RpcException({
+        code: 500,
+        message: `Error to get the collaborator with the provided ID: ${error}`,
       });
     }
   }
