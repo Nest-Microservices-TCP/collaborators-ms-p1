6ada4752f73fbf77eab953b3e35355c1f25dbfea
JosueEC <josueev.cruz@gmail.com>
2024-08-28 18:36:57 -0600

    [FEAT](collaborators.controller): Expose the find-on-by-id method

--- src/collaborators/collaborators.controller.ts
+++ src/collaborators/collaborators.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { CollaboratorsService } from './collaborators.service';
 import { CollaboratorEntity } from './entities/collaborator.entity';
+import { FindOneCollaboratorById } from './dto/find-one-collaborator-by-id.dto';
 
 @Controller()
 export class CollaboratorsController {
@@ -8,5 +9,11 @@ export class CollaboratorsController {
 
   async findAll(): Promise<CollaboratorEntity[]> {
     return this.collaboratorsService.findAll();
+  }
+
+  async findOneById(
+    request: FindOneCollaboratorById,
+  ): Promise<CollaboratorEntity> {
+    return this.collaboratorsService.findOneById(request);
   }
 }
