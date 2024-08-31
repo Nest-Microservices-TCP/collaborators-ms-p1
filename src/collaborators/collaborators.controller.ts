8ff98ccb03ce9db2a027a659443572bf96ef0620
JosueEC <josueev.cruz@gmail.com>
2024-08-30 20:02:22 -0600

    [FEAT](collaborators.controller): Expose the save method

--- src/collaborators/collaborators.controller.ts
+++ src/collaborators/collaborators.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { CollaboratorsService } from './collaborators.service';
 import { CollaboratorEntity } from './entities/collaborator.entity';
 import { FindOneCollaboratorById } from './dto/find-one-collaborator-by-id.dto';
+import { CreateCollaboratorDto } from './dto';
 
 @Controller()
 export class CollaboratorsController {
@@ -15,5 +16,9 @@ export class CollaboratorsController {
     request: FindOneCollaboratorById,
   ): Promise<CollaboratorEntity> {
     return this.collaboratorsService.findOneById(request);
+  }
+
+  async save(request: CreateCollaboratorDto) {
+    return this.collaboratorsService.save(request);
   }
 }
