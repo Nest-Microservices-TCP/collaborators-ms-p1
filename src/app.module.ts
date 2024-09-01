0ab6ec24f32c66eca41ae768ab9918f8a3b81c02
JosueEC <josueev.cruz@gmail.com>
2024-08-31 21:19:56 -0600

    [FEAT](app.module): Add the postgresql-module to the imports array

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
 import { AreasModule } from './areas/areas.module';
+import { PostgresDBModule } from './database/postgresdb.module';
 
 @Module({
-  imports: [CollaboratorsModule, AreasModule],
+  imports: [PostgresDBModule, CollaboratorsModule, AreasModule],
   controllers: [],
   providers: [],
 })
