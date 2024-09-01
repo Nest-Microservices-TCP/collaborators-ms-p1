f3b89eee28b50b07e61915b635d1fe416a9f5968
JosueEC <josueev.cruz@gmail.com>
2024-08-31 21:20:26 -0600

    [FEAT](areas.module): Add the areas-repository to the providers array

--- src/areas/areas.module.ts
+++ src/areas/areas.module.ts
@@ -3,11 +3,11 @@ import { AreasController } from './areas.controller';
 import { AreasService } from './areas.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AreaEntity } from './entities/area.entity';
-import { UnitOfWork } from 'src/common/unit-of-work/unit-of-work.service';
+import { AreasRepository } from './repositories/areas.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AreaEntity])],
   controllers: [AreasController],
-  providers: [AreasService, UnitOfWork],
+  providers: [AreasService, AreasRepository],
 })
 export class AreasModule {}
