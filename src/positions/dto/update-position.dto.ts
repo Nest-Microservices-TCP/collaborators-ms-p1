15e749c3049118ef78900a62e1673973204806a7
JosueEC <josueev.cruz@gmail.com>
2024-09-04 22:07:52 -0600

    [FEAT](positions/dto): Define the properties for update-position dto

--- src/positions/dto/update-position.dto.ts
+++ src/positions/dto/update-position.dto.ts
@@ -1,1 +1,15 @@
-export class UpdatePositionDto {}
+import { IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
+
+export class UpdatePositionDto {
+  @IsUUID()
+  positionId: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(255)
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
