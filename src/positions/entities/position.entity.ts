198f896028466b71b78c72e00bd9e1c9006cb40b
JosueEC <josueev.cruz@gmail.com>
2024-09-04 22:07:20 -0600

    [FEAT](positions/entities): Define the properties for position-entity

--- src/positions/entities/position.entity.ts
+++ src/positions/entities/position.entity.ts
@@ -1,5 +1,20 @@
 import { BaseEntity } from 'src/common/entity/BaseEntity';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity({ name: 'positions' })
-export class PositionEntity extends BaseEntity {}
+export class PositionEntity extends BaseEntity {
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    name: 'description',
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+}
