c7ecee987df750e8387e88f4d1df27f09e84f86b
JosueEC <josueev.cruz@gmail.com>
2024-09-04 22:10:00 -0600

    [FEAT](positions.service): Add the find-all method and use the handle-rpc-exceptions decorator

--- src/positions/positions.service.ts
+++ src/positions/positions.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
 import { PositionsRepository } from './repositories/positions.repository';
+import { PositionEntity } from './entities/position.entity';
+import { Injectable } from '@nestjs/common';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
 @Injectable()
 export class PositionsService {
   constructor(private readonly positionsRepository: PositionsRepository) {}
+
+  @HandleRpcExceptions()
+  findAll(): Promise<PositionEntity[]> {
+    return this.positionsRepository.findAll();
+  }
 }
