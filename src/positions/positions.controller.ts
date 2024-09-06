2ed570d8936afa443a3988b6f1f0a56ad1b242d4
JosueEC <josueev.cruz@gmail.com>
2024-09-05 18:56:30 -0600

    [FEAT](positions.controller): Expose the find-all method

--- src/positions/positions.controller.ts
+++ src/positions/positions.controller.ts
@@ -1,7 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { PositionsService } from './positions.service';
+import { PositionEntity } from './entities/position.entity';
 
 @Controller()
 export class PositionsController {
   constructor(private readonly positionsService: PositionsService) {}
+
+  findAll(): Promise<PositionEntity[]> {
+    return this.positionsService.findAll();
+  }
 }
