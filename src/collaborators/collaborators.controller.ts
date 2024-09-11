15f8c214030b5f6013b2766f9f481cecf226b495
JosueEC <josueev.cruz@gmail.com>
2024-09-10 20:08:10 -0600

    [FEAT](collaborators.controller): Expose all the methods as microservice communication

--- src/collaborators/collaborators.controller.ts
+++ src/collaborators/collaborators.controller.ts
@@ -4,26 +4,40 @@ import { CollaboratorEntity } from './entities/collaborator.entity';
 import { FindOneCollaboratorById } from './dto/find-one-collaborator-by-id.dto';
 import { CreateCollaboratorDto } from './dto';
 import { UpdateCollaboratorDto } from './dto/update-collaborator.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class CollaboratorsController {
   constructor(private readonly collaboratorsService: CollaboratorsService) {}
 
+  @MessagePattern({ cmd: 'find.all.collaborators' })
   async findAll(): Promise<CollaboratorEntity[]> {
     return this.collaboratorsService.findAll();
   }
 
+  @MessagePattern({ cmd: 'find.one.collaborator.by.id' })
   async findOneById(
-    request: FindOneCollaboratorById,
+    @Payload() request: FindOneCollaboratorById,
   ): Promise<CollaboratorEntity> {
     return this.collaboratorsService.findOneById(request);
   }
 
-  async save(request: CreateCollaboratorDto): Promise<CollaboratorEntity> {
+  @MessagePattern({ cmd: 'save.collaborator' })
+  async save(
+    @Payload() request: CreateCollaboratorDto,
+  ): Promise<CollaboratorEntity> {
     return this.collaboratorsService.save(request);
   }
 
-  async update(request: UpdateCollaboratorDto): Promise<CollaboratorEntity> {
+  @MessagePattern({ cmd: 'update.collaborator' })
+  async update(
+    @Payload() request: UpdateCollaboratorDto,
+  ): Promise<CollaboratorEntity> {
     return this.collaboratorsService.update(request);
+  }
+
+  @MessagePattern({ cmd: 'delete.collaborator.by.id' })
+  async deleteById(@Payload('id') id: string): Promise<CollaboratorEntity> {
+    return this.collaboratorsService.deleteById(id);
   }
 }
