edd5e20e30d213c0956e38538bd8a3f4ea93560c
JosueEC <josueev.cruz@gmail.com>
2024-09-19 19:46:27 -0600

    [FIX](collaborators.repository.interface): Use the correct import path for request dtos

--- src/collaborators/repositories/interfaces/collaborators.repository.interface.ts
+++ src/collaborators/repositories/interfaces/collaborators.repository.interface.ts
@@ -1,7 +1,9 @@
-import { CreateCollaboratorDto } from 'src/collaborators/dto';
-import { UpdateCollaboratorDto } from 'src/collaborators/dto/update-collaborator.dto';
 import { CollaboratorEntity } from 'src/collaborators/entities/collaborator.entity';
 import { IBaseRepository } from 'src/common/interfaces';
+import {
+  CreateCollaboratorDto,
+  UpdateCollaboratorDto,
+} from 'src/collaborators/dto/request';
 
 export interface ICollaboratorsRepository
   extends IBaseRepository<
