dd177684c0f699c86c579c895ed23746791e523d
JosueEC <josueev.cruz@gmail.com>
2024-09-20 19:43:38 -0600

    [FIX](positions.repository.interface): Use the correct path import for request dtos

--- src/positions/repositories/interfaces/positions.repository.interface.ts
+++ src/positions/repositories/interfaces/positions.repository.interface.ts
@@ -1,5 +1,8 @@
 import { IBaseRepository } from 'src/common/interfaces';
-import { CreatePositionDto, UpdatePositionDto } from 'src/positions/dto';
+import {
+  CreatePositionDto,
+  UpdatePositionDto,
+} from 'src/positions/dto/request';
 import { PositionEntity } from 'src/positions/entities/position.entity';
 
 export interface IPositionsRepository
