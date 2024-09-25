b089047a585e75060841bceafbae5f2775b368b1
JosueEC <josueev.cruz@gmail.com>
2024-09-24 20:39:22 -0600

    [FEAT](work-shifts): Add the work-shifts service with find-all method

---
+++ src/work-shifts/work-shifts.service.ts
@@ -0,0 +1,19 @@
+import { plainToInstance } from 'class-transformer';
+import { WorkShiftResponseDto } from './dto/response';
+import { WorkShiftsRepository } from './repositories/work-shifts.repository';
+import { HandleRpcExceptions } from 'src/common/decorators';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class WorkShiftsService {
+  constructor(private readonly workShiftsRepository: WorkShiftsRepository) {}
+
+  @HandleRpcExceptions()
+  async findAll(): Promise<WorkShiftResponseDto[]> {
+    const workShifts = await this.workShiftsRepository.findAll();
+
+    return plainToInstance(WorkShiftResponseDto, workShifts, {
+      excludeExtraneousValues: true,
+    });
+  }
+}
