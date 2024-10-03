9bfb57873e5920327102522f1e797722e0969453
JosueEC <josueev.cruz@gmail.com>
2024-10-02 19:06:39 -0600

    [REFACTOR](area.entity): Add the area-id as primary key

--- src/areas/entity/area.entity.ts
+++ src/areas/entity/area.entity.ts
@@ -1,8 +1,13 @@
 import { BaseEntity } from 'src/common/entity/BaseEntity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'areas' })
 export class AreaEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid', {
+    name: 'area_id',
+  })
+  area_id: string;
+
   @Column({
     name: 'name',
     type: 'varchar',
