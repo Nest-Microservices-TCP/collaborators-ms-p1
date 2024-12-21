9edf0faaab0e9c92c36d9c76e26447f006940d87
JosueEC <josueev.cruz@gmail.com>
2024-12-20 21:40:26 -0600

    [REFACTOR](work-shifts.service): Use the plain-to-instance-dto method to transform all methods responses

--- src/work-shifts/work-shifts.service.ts
+++ src/work-shifts/work-shifts.service.ts
@@ -20,36 +20,35 @@ export class WorkShiftsService {
   async findAll(): Promise<WorkShiftResponseDto[]> {
     const workShifts = await this.workShiftsRepository.findAll();
 
-    return plainToInstance(WorkShiftResponseDto, workShifts, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(workShifts);
   }
 
   @HandleRpcExceptions()
   async findOne(workShiftId: string): Promise<WorkShiftResponseDto> {
     const workShift = await this.workShiftsRepository.findOne(workShiftId);
 
-    return plainToInstance(WorkShiftResponseDto, workShift, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(workShift);
+  }
+
+  @HandleRpcExceptions()
+  async findByIds(workShiftsIds: string[]): Promise<WorkShiftResponseDto[]> {
+    const workShifts = await this.workShiftsRepository.findByIds(workShiftsIds);
+
+    return this.plainToInstanceDto(workShifts);
   }
 
   @HandleRpcExceptions()
   async save(request: CreateWorkShiftDto): Promise<WorkShiftResponseDto> {
-    const workShift = await this.workShiftsRepository.save(request);
+    const newWorkShift = await this.workShiftsRepository.save(request);
 
-    return plainToInstance(WorkShiftResponseDto, workShift, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(newWorkShift);
   }
 
   @HandleRpcExceptions()
   async update(request: UpdateWorkShiftDto): Promise<WorkShiftResponseDto> {
-    const workShift = await this.workShiftsRepository.update(request);
+    const updatedWorkShift = await this.workShiftsRepository.update(request);
 
-    return plainToInstance(WorkShiftResponseDto, workShift, {
-      excludeExtraneousValues: true,
-    });
+    return this.plainToInstanceDto(updatedWorkShift);
   }
 
   @HandleRpcExceptions()
@@ -57,15 +56,6 @@ export class WorkShiftsService {
     const deleteResult = await this.workShiftsRepository.remove(workShiftId);
 
     return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(workShiftsIds: string[]): Promise<WorkShiftResponseDto[]> {
-    const workShifts = await this.workShiftsRepository.findByIds(workShiftsIds);
-
-    return plainToInstance(WorkShiftResponseDto, workShifts, {
       excludeExtraneousValues: true,
     });
   }
