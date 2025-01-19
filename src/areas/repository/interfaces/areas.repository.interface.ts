32d8be6f2a72d9d6a9175325925dea52ecb94b0e
JosueEC <josueev.cruz@gmail.com>
2025-01-18 19:41:45 -0600

    [REFACTOR](areas.repository-interface): Remove the update dto from the interface params

--- src/areas/repository/interfaces/areas.repository.interface.ts
+++ src/areas/repository/interfaces/areas.repository.interface.ts
@@ -1,6 +1,5 @@
-import { CreateAreaDto, UpdateAreaDto } from 'src/areas/dto/request';
+import { CreateAreaDto } from 'src/areas/dto/request';
 import { Area } from 'src/areas/entity/area.entity';
 import { IRepository } from 'src/common/repository';
 
-export interface IAreasRepository
-  extends IRepository<Area, CreateAreaDto, UpdateAreaDto> {}
+export interface IAreasRepository extends IRepository<Area, CreateAreaDto> {}
