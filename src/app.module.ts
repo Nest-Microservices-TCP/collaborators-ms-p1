a40a4652552d54492f48a7eb3aaf4e3509763158
JosueEC <josueev.cruz@gmail.com>
2025-01-19 10:27:18 -0600

    [FEAT](app.module): Sort and group the imports sentences

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { AreasModule } from './areas/areas.module';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
-import { WorkShiftsModule } from './work-shifts/work-shifts.module';
 import { PostgresDBModule } from './database/postgresdb.module';
 import { PositionsModule } from './positions/positions.module';
-import { AreasModule } from './areas/areas.module';
-import { Module } from '@nestjs/common';
+import { WorkShiftsModule } from './work-shifts/work-shifts.module';
 
 @Module({
   imports: [
