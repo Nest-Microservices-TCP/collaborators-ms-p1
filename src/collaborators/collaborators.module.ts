a038c07a93fae55eeaceee752290dc3f9445e8e9
JosueEC <josueev.cruz@gmail.com>
2025-01-19 10:28:38 -0600

    [REFACTOR](collaborators.module): Sort and group the imports sentences

--- src/collaborators/collaborators.module.ts
+++ src/collaborators/collaborators.module.ts
@@ -1,9 +1,11 @@
-import { CollaboratorsRepository } from './repository/collaborators.repository';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Collaborator } from './entity/collaborator.entity';
+
 import { CollaboratorsController } from './collaborators.controller';
 import { CollaboratorsService } from './collaborators.service';
-import { Collaborator } from './entity/collaborator.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
+import { CollaboratorsRepository } from './repository/collaborators.repository';
 @Module({
   imports: [TypeOrmModule.forFeature([Collaborator])],
   controllers: [CollaboratorsController],
