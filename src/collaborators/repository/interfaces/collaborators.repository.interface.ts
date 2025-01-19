478f1779dd6267e7bd1793f9c6e23b4792d60701
JosueEC <josueev.cruz@gmail.com>
2025-01-18 19:49:57 -0600

    [REFACTOR](collaborators.repository.interface): Remove the update dto from the interface params

--- src/collaborators/repository/interfaces/collaborators.repository.interface.ts
+++ src/collaborators/repository/interfaces/collaborators.repository.interface.ts
@@ -1,13 +1,6 @@
-import {
-  CreateCollaboratorDto,
-  UpdateCollaboratorDto,
-} from 'src/collaborators/dto/request';
+import { CreateCollaboratorDto } from 'src/collaborators/dto/request';
 import { Collaborator } from 'src/collaborators/entity/collaborator.entity';
 import { IRepository } from 'src/common/repository';
 
 export interface ICollaboratorsRepository
-  extends IRepository<
-    Collaborator,
-    CreateCollaboratorDto,
-    UpdateCollaboratorDto
-  > {}
+  extends IRepository<Collaborator, CreateCollaboratorDto> {}
