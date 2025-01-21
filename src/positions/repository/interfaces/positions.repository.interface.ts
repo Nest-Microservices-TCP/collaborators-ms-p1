5af545a6dd4a65d0edce96a37b3b1495477a93d4
JosueEC <josueev.cruz@gmail.com>
2025-01-20 21:54:27 -0600

    [FEAT](positions.repository.interface): Remove the update dto from the interface params

--- src/positions/repository/interfaces/positions.repository.interface.ts
+++ src/positions/repository/interfaces/positions.repository.interface.ts
@@ -1,9 +1,6 @@
 import { IRepository } from 'src/common/repository';
-import {
-  CreatePositionDto,
-  UpdatePositionDto,
-} from 'src/positions/dto/request';
+import { CreatePositionDto } from 'src/positions/dto/request';
 import { Position } from 'src/positions/entity/position.entity';
 
 export interface IPositionsRepository
-  extends IRepository<Position, CreatePositionDto, UpdatePositionDto> {}
+  extends IRepository<Position, CreatePositionDto> {}
