4bd7223a99491d39d27121ceed18e8118380427a
JosueEC <josueev.cruz@gmail.com>
2025-02-21 20:46:00 -0600

    [REFACTOR](areas.controller): Replace the collaborators topic by the positions topic

--- src/positions/positions.controller.ts
+++ src/positions/positions.controller.ts
@@ -11,40 +11,40 @@ import { PositionsService } from './positions.service';
 export class PositionsController {
   constructor(private readonly positionsService: PositionsService) {}
 
-  @MessagePattern('collaborators.find.all.positions')
+  @MessagePattern('positions.find.all')
   async findAll(): Promise<PositionResponseDto[]> {
     return this.positionsService.findAll();
   }
 
-  @MessagePattern('collaborators.find.one.position')
+  @MessagePattern('positions.find.one')
   async findOne(
     @Payload('positionId') positionId: string,
   ): Promise<PositionResponseDto> {
     return this.positionsService.findOne(positionId);
   }
 
-  @MessagePattern('collaborators.find.positions.by.ids')
+  @MessagePattern('positions.find.by.ids')
   async findByIds(
     @Payload('positionsIds') positionsIds: string[],
   ): Promise<PositionResponseDto[]> {
     return this.positionsService.findByIds(positionsIds);
   }
 
-  @MessagePattern('collaborators.save.position')
+  @MessagePattern('positions.save')
   async save(
     @Payload() request: CreatePositionDto,
   ): Promise<PositionResponseDto> {
     return this.positionsService.save(request);
   }
 
-  @MessagePattern('collaborators.update.position')
+  @MessagePattern('positions.update')
   async update(
     @Payload() request: UpdatePositionDto,
   ): Promise<PositionResponseDto> {
     return this.positionsService.update(request);
   }
 
-  @MessagePattern('collaborators.remove.position')
+  @MessagePattern('positions.remove')
   async remove(
     @Payload('positionId') positionId: string,
   ): Promise<DeleteResultResponse> {
