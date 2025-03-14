76ae6c424f96360f9eeeb2e5a2fc87a8cba0e08b
JosueEC <josueev.cruz@gmail.com>
2025-03-13 22:01:12 -0600

    [REFACTOR](areas.controller): Use the types generated by the proto files

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -1,47 +1,34 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateAreaDto, UpdateAreaDto } from './dto/request';
-import { AreaResponseDto } from './dto/response';
+import {
+  Area,
+  CreateAreaRequest,
+  FindAreasResponse,
+  FindOneAreaRequest,
+  AreasServiceController,
+  AreasServiceControllerMethods,
+} from 'src/grpc/proto/collaborators/areas.pb';
 
 import { AreasService } from './areas.service';
 
 @Controller()
-export class AreasController {
+@AreasServiceControllerMethods()
+export class AreasController implements AreasServiceController {
   constructor(private readonly areasService: AreasService) {}
 
-  @MessagePattern('areas.save')
-  async save(@Payload() request: CreateAreaDto): Promise<AreaResponseDto> {
-    return this.areasService.save(request);
+  save(request: CreateAreaRequest): void {
+    this.areasService.save(request);
   }
-
-  @MessagePattern('areas.find.all')
-  async findAll(): Promise<AreaResponseDto[]> {
-    return this.areasService.findAll();
+  find():
+    | Promise<FindAreasResponse>
+    | Observable<FindAreasResponse>
+    | FindAreasResponse {
+    return this.areasService.find();
   }
-
-  @MessagePattern('areas.find.one')
-  async findOne(@Payload('areaId') areaId: string): Promise<AreaResponseDto> {
-    return this.areasService.findOne(areaId);
-  }
-
-  @MessagePattern('areas.update')
-  async update(@Payload() request: UpdateAreaDto): Promise<AreaResponseDto> {
-    return this.areasService.update(request);
-  }
-
-  @MessagePattern('areas.remove')
-  async remove(
-    @Payload('areaId') areaId: string,
-  ): Promise<DeleteResultResponse> {
-    return this.areasService.remove(areaId);
-  }
-
-  @MessagePattern('areas.find.by.ids')
-  async findByIds(
-    @Payload('areasIds') areasIds: string[],
-  ): Promise<AreaResponseDto[]> {
-    return this.areasService.findByIds(areasIds);
+  findOne(
+    request: FindOneAreaRequest,
+  ): Promise<Area> | Observable<Area> | Area {
+    return this.areasService.findOne(request);
   }
 }
