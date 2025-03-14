3108afcdb0d94ce590e177117363d7aced7b4ae9
JosueEC <josueev.cruz@gmail.com>
2025-03-13 21:56:19 -0600

    [REFACTOR](areas/dto/response): Delete the response dtos

--- src/areas/dto/response/area.response.dto.ts
+++
@@ -1,7 +0,0 @@
-import { BaseResponseDto } from 'src/common/dto/response';
-import { Expose } from 'class-transformer';
-
-export class AreaResponseDto extends BaseResponseDto {
-  @Expose()
-  name: string;
-}
