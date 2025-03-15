dbc29df46e3f07b2e0922de8bf473231bbce3c72
JosueEC <josueev.cruz@gmail.com>
2025-03-14 20:33:10 -0600

    [REFACTOR](collaborators.service): Use the types generated by the proto files

--- src/collaborators/collaborators.service.ts
+++ src/collaborators/collaborators.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
-
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
 import {
-  CreateCollaboratorDto,
-  FindOneCollaboratorById,
-  UpdateCollaboratorDto,
-} from './dto/request';
-import { CollaboratorResponseDto } from './dto/response';
+  CreateCollaboratorRequest,
+  FindCollaboratorsResponse,
+  FindOneCollaboratorRequest,
+} from 'src/grpc/proto/collaborators/collaborators.pb';
 
 import { CollaboratorsRepository } from './repository/collaborators.repository';
+import { Collaborator } from './entity/collaborator.entity';
 
 @Injectable()
 export class CollaboratorsService {
@@ -19,69 +16,20 @@ export class CollaboratorsService {
     private readonly collaboratorsRepository: CollaboratorsRepository,
   ) {}
 
-  private plainToInstanceDto(data: unknown): any {
-    return plainToInstance(CollaboratorResponseDto, data, {
-      excludeExtraneousValues: true,
-    });
+  @HandleRpcExceptions()
+  async find(): Promise<FindCollaboratorsResponse> {
+    const collaborators = await this.collaboratorsRepository.find();
+
+    return { collaborators };
   }
 
   @HandleRpcExceptions()
-  async findAll(): Promise<CollaboratorResponseDto[]> {
-    const collaborators = await this.collaboratorsRepository.findAll();
-
-    return this.plainToInstanceDto(collaborators);
+  async findOne(request: FindOneCollaboratorRequest): Promise<Collaborator> {
+    return this.collaboratorsRepository.findOne(request);
   }
 
   @HandleRpcExceptions()
-  async findOne(
-    request: FindOneCollaboratorById,
-  ): Promise<CollaboratorResponseDto> {
-    const { collaboratorId } = request;
-
-    const collaborator =
-      await this.collaboratorsRepository.findOne(collaboratorId);
-
-    return this.plainToInstanceDto(collaborator);
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(
-    collaboratorsIds: string[],
-  ): Promise<CollaboratorResponseDto[]> {
-    const collaborators =
-      await this.collaboratorsRepository.findByIds(collaboratorsIds);
-
-    return this.plainToInstanceDto(collaborators);
-  }
-
-  @HandleRpcExceptions()
-  async save(request: CreateCollaboratorDto): Promise<CollaboratorResponseDto> {
-    const newCollaborator = await this.collaboratorsRepository.save(request);
-
-    return this.plainToInstanceDto(newCollaborator);
-  }
-
-  @HandleRpcExceptions()
-  async update(
-    request: UpdateCollaboratorDto,
-  ): Promise<CollaboratorResponseDto> {
-    const { collaboratorId, ...rest } = request;
-
-    const updatedCollaborator = await this.collaboratorsRepository.update(
-      { collaboratorId },
-      rest,
-    );
-
-    return this.plainToInstanceDto(updatedCollaborator);
-  }
-
-  @HandleRpcExceptions()
-  async remove(collaboratorId: string): Promise<DeleteResultResponse> {
-    const deleteResult =
-      await this.collaboratorsRepository.remove(collaboratorId);
-
-    return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
+  async save(request: CreateCollaboratorRequest): Promise<void> {
+    this.collaboratorsRepository.save(request);
   }
 }
