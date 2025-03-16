58b5a065d0b2fffa688741d6b36c33f8f1f87a48
JosueEC <josueev.cruz@gmail.com>
2025-03-15 22:41:38 -0600

    [REFACTOR](positions.service): Use the types generated by the proto files

--- src/positions/positions.service.ts
+++ src/positions/positions.service.ts
@@ -1,70 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
-
 import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreatePositionDto, UpdatePositionDto } from './dto/request';
-import { PositionResponseDto } from './dto/response';
+import {
+  FindPositionsResponse,
+  CreatePositionRequest,
+  FindOnePositionRequest,
+} from 'src/grpc/proto/collaborators/positions.pb';
 
 import { PositionsRepository } from './repository/positions.repository';
+
+import { Position } from './entity/position.entity';
 
 @Injectable()
 export class PositionsService {
   constructor(private readonly positionsRepository: PositionsRepository) {}
 
-  private plainToInstanceDto(data: unknown): any {
-    return plainToInstance(PositionResponseDto, data, {
-      excludeExtraneousValues: true,
-    });
+  @HandleRpcExceptions()
+  async save(request: CreatePositionRequest): Promise<void> {
+    this.positionsRepository.save(request);
   }
 
   @HandleRpcExceptions()
-  async findAll(): Promise<PositionResponseDto[]> {
-    const positions = await this.positionsRepository.findAll();
+  async find(): Promise<FindPositionsResponse> {
+    const positions = await this.positionsRepository.find();
 
-    return this.plainToInstanceDto(positions);
+    return { positions };
   }
 
   @HandleRpcExceptions()
-  async findOne(positionId: string): Promise<PositionResponseDto> {
-    const position = await this.positionsRepository.findOne(positionId);
-
-    return this.plainToInstanceDto(position);
-  }
-
-  @HandleRpcExceptions()
-  async findByIds(positionsIds: string[]): Promise<PositionResponseDto[]> {
-    const positions = await this.positionsRepository.findByIds(positionsIds);
-
-    return this.plainToInstanceDto(positions);
-  }
-
-  @HandleRpcExceptions()
-  async save(request: CreatePositionDto): Promise<PositionResponseDto> {
-    const newPosition = await this.positionsRepository.save(request);
-
-    return this.plainToInstanceDto(newPosition);
-  }
-
-  @HandleRpcExceptions()
-  async update(request: UpdatePositionDto): Promise<PositionResponseDto> {
-    const { positionId, ...rest } = request;
-
-    const updatedPosition = await this.positionsRepository.update(
-      { positionId },
-      rest,
-    );
-
-    return this.plainToInstanceDto(updatedPosition);
-  }
-
-  @HandleRpcExceptions()
-  async remove(positionId: string): Promise<DeleteResultResponse> {
-    const deleteResult = await this.positionsRepository.remove(positionId);
-
-    return plainToInstance(DeleteResultResponse, deleteResult, {
-      excludeExtraneousValues: true,
-    });
+  async findOne(request: FindOnePositionRequest): Promise<Position> {
+    return this.positionsRepository.findOne(request);
   }
 }
