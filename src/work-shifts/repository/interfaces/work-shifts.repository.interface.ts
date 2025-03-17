b4d05df15b80714b0761550e679370bec249074a
JosueEC <josueev.cruz@gmail.com>
2025-03-16 22:52:50 -0600

    [FEAT](work-shifts.repository.interface): Use the types generated by the proto files

--- src/work-shifts/repository/interfaces/work-shifts.repository.interface.ts
+++ src/work-shifts/repository/interfaces/work-shifts.repository.interface.ts
@@ -1,6 +1,13 @@
 import { IRepository } from 'src/common/repository';
-import { CreateWorkShiftDto } from 'src/work-shifts/dto/request';
+import {
+  CreateWorkShiftRequest,
+  FindOneWorkShiftRequest,
+} from 'src/grpc/proto/collaborators/work_shifts.pb';
 import { WorkShift } from 'src/work-shifts/entity/work-shift.entity';
 
 export interface IWorkShiftsRepository
-  extends IRepository<WorkShift, CreateWorkShiftDto> {}
+  extends IRepository<
+    WorkShift,
+    CreateWorkShiftRequest,
+    FindOneWorkShiftRequest
+  > {}
