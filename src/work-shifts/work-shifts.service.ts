3594f83b21217acf473131cf019eeba3672efc8e
JosueEC <josueev.cruz@gmail.com>
2025-03-16 22:58:09 -0600

    [FEAT](work-shifts.service): Add the find-one service method

--- src/work-shifts/work-shifts.service.ts
+++ src/work-shifts/work-shifts.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { HandleRpcExceptions } from 'src/common/decorators';
 
-import { CreateWorkShiftRequest } from 'src/grpc/proto/collaborators/work_shifts.pb';
+import {
+  CreateWorkShiftRequest,
+  FindOneWorkShiftRequest,
+} from 'src/grpc/proto/collaborators/work_shifts.pb';
 
 import { WorkShiftsRepository } from './repository/work-shifts.repository';
+
+import { WorkShift } from './entity/work-shift.entity';
 
 @Injectable()
 export class WorkShiftsService {
   constructor(private readonly workShiftsRepository: WorkShiftsRepository) {}
 
+  @HandleRpcExceptions()
   async save(request: CreateWorkShiftRequest): Promise<void> {
     this.workShiftsRepository.save(request);
+  }
+
+  @HandleRpcExceptions()
+  async findOne(request: FindOneWorkShiftRequest): Promise<WorkShift> {
+    return this.workShiftsRepository.findOne(request);
   }
 }
