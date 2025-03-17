c49d910693359bf3cc7e63a70adeab77c8801f51
JosueEC <josueev.cruz@gmail.com>
2025-03-16 22:53:55 -0600

    [REFACTOR](work-shifts.module): Sort and group the imports sentences

--- src/work-shifts/work-shifts.module.ts
+++ src/work-shifts/work-shifts.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { WorkShift } from './entity/work-shift.entity';
-
-import { WorkShiftsRepository } from './repository/work-shifts.repository';
-import { WorkShiftsController } from './work-shifts.controller';
 import { WorkShiftsService } from './work-shifts.service';
+import { WorkShiftsController } from './work-shifts.controller';
+import { WorkShiftsRepository } from './repository/work-shifts.repository';
+
+import { WorkShift } from './entity/work-shift.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([WorkShift])],
-  controllers: [WorkShiftsController],
   providers: [WorkShiftsRepository, WorkShiftsService],
+  controllers: [WorkShiftsController],
 })
 export class WorkShiftsModule {}
