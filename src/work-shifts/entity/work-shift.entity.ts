73285450edc330a448834081d05e25b82f1ab45d
JosueEC <josueev.cruz@gmail.com>
2025-03-16 22:53:19 -0600

    [REFACTOR](work-shift.entity): Use the types generated by the proto files

--- src/work-shifts/entity/work-shift.entity.ts
+++ src/work-shifts/entity/work-shift.entity.ts
@@ -1,12 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { WorkShift as IWorkShift } from 'src/grpc/proto/collaborators/work_shifts.pb';
 import { BaseEntity } from 'src/common/entity';
 
 @Entity({ name: 'work_shifts' })
-export class WorkShift extends BaseEntity {
+export class WorkShift extends BaseEntity implements IWorkShift {
   @PrimaryGeneratedColumn('uuid', {
     name: 'work_shift_id',
   })
-  workShiftId: string;
+  work_shift_id: string;
 
   @Column({
     name: 'name',
@@ -22,19 +23,19 @@ export class WorkShift extends BaseEntity {
     type: 'time with time zone',
     nullable: false,
   })
-  checkInTime: Date;
+  check_in_time: string;
 
   @Column({
     name: 'departure_time',
     type: 'time with time zone',
     nullable: true,
   })
-  departureTime: Date;
+  departure_time: string;
 
   @Column({
     name: 'is_shift_open',
     type: 'bool',
     default: false,
   })
-  isShiftOpen: boolean;
+  is_shift_open: boolean;
 }
