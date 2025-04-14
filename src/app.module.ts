9a1a8120a3f1a8282a0378dcdea15cabb456890e
JosueEC <josueev.cruz@gmail.com>
2025-04-13 22:06:03 -0600

    [CHORE](app.module): Sort and group the imports sentences

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { PostgresDBModule } from './database/postgresdb.module';
+
 import { AreasModule } from './areas/areas.module';
 import { PositionsModule } from './positions/positions.module';
-import { PostgresDBModule } from './database/postgresdb.module';
 import { WorkShiftsModule } from './work-shifts/work-shifts.module';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
 
