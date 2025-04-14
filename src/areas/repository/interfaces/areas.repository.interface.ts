6052ee40b71c1c62ba076230edf3ded2be665ece
JosueEC <josueev.cruz@gmail.com>
2025-04-13 22:08:42 -0600

    [FIX](areas.repository.interface): Fix the path for the proto files imports

--- src/areas/repository/interfaces/areas.repository.interface.ts
+++ src/areas/repository/interfaces/areas.repository.interface.ts
@@ -3,7 +3,7 @@ import { IRepository } from 'src/common/repository';
 import {
   CreateAreaRequest,
   FindOneAreaRequest,
-} from 'src/grpc/proto-files/collaborators/areas.pb';
+} from 'src/grpc/collaborators/areas.pb';
 
 export interface IAreasRepository
   extends IRepository<Area, CreateAreaRequest, FindOneAreaRequest> {}
